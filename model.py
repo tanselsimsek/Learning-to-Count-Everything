6a47e6039dc893e4f2d0e923a2799f9989ad962a
juanmatanaranjo <juan.mnaranjo@hotmail.com>
2021-12-10 19:16:33 +0100

    Include models with YOLO

--- model.py
+++ model.py
@@ -1,12 +1,62 @@
 import torch, torchvision
+import torchvision.models as models
 import torch.nn as nn
 import torch.nn.functional as F
 from collections import OrderedDict
 
 
-class Resnet50FPN(nn.Module):
+class Wide_Resnet50_2(nn.Module):
     def __init__(self):
-        super(Resnet50FPN, self).__init__()
+        super(Wide_Resnet50_2, self).__init__()
+        #Loading pre-trained Resnet 50
+        self.resnet = torchvision.models.wide_resnet50_2(pretrained= True)
+        #list of all the children layer of resnet 50
+        children = list(self.resnet.children())
+        #creating a new sequential model with children 4 as the first layer
+        self.conv1 = nn.Sequential(*children[:4])
+        self.conv2 = children[4]
+        self.conv3 = children[5]
+        self.conv4 = children[6]
+    def forward(self, im_data):
+        feat = OrderedDict()
+        #generating different features maps according to the convolutional layer
+        feat_map = self.conv1(im_data)
+        feat_map = self.conv2(feat_map)
+        feat_map3 = self.conv3(feat_map)
+        feat_map4 = self.conv4(feat_map3)
+        #storing activation maps into an ordered dict
+        feat['map3'] = feat_map3
+        feat['map4'] = feat_map4
+        return feat
+
+class VGG16FPN(nn.Module):
+    def __init__(self):
+        super(VGG16FPN, self).__init__()
+        vgg16 = torchvision.models.vgg16(pretrained=True)
+        children = list(vgg16.children())[0]
+        self.conv1 = nn.Sequential(children[:5])
+        self.conv2 = nn.Sequential(children[5:10])
+        self.conv3 = nn.Sequential(children[10:14])
+        self.conv4 = nn.Sequential(children[14:19])
+        self.conv5 = nn.Sequential(children[19:24])
+        self.conv6 = nn.Sequential(children[24:])
+    def forward(self, im_data):
+        feat = OrderedDict()
+        #generating different features maps according to the convolutional layer
+        feat_map = self.conv1(im_data)
+        feat_map = self.conv2(feat_map)
+        feat_map = self.conv3(feat_map)
+        feat_map = self.conv4(feat_map)
+        feat_map3 = self.conv5(feat_map)
+        feat_map4 = self.conv6(feat_map3)
+        #storing activation maps into an ordered dict
+        feat['map3'] = feat_map3
+        feat['map4'] = feat_map4
+        return feat
+        
+class Resnet50FPN(nn.Module):
+    def _init_(self):
+        super(Resnet50FPN, self)._init_()
         self.resnet = torchvision.models.resnet50(pretrained=True)
         children = list(self.resnet.children())
         self.conv1 = nn.Sequential(*children[:4])
@@ -24,6 +74,7 @@ class Resnet50FPN(nn.Module):
         return feat
 
 
+#this class is the upsampling block (usign UpsamplingBilinear2d)
 class CountRegressor(nn.Module):
     def __init__(self, input_channels,pool='mean'):
         super(CountRegressor, self).__init__()
@@ -31,6 +82,8 @@ class CountRegressor(nn.Module):
         self.regressor = nn.Sequential(
             nn.Conv2d(input_channels, 196, 7, padding=3),
             nn.ReLU(),
+            #Applies a 2D bilinear upsampling to an input signal composed of several input channels
+            #given a tensor of size 2, UpsamplingBilinear2d(scale_factor=2) return an array of size 4
             nn.UpsamplingBilinear2d(scale_factor=2),
             nn.Conv2d(196, 128, 5, padding=2),
             nn.ReLU(),
@@ -48,9 +101,11 @@ class CountRegressor(nn.Module):
         num_sample =  im.shape[0]
         if num_sample == 1:
             output = self.regressor(im.squeeze(0))
+            #Average Pooling is a pooling operation that calculates the average value for patches of a feature map
             if self.pool == 'mean':
                 output = torch.mean(output, dim=(0),keepdim=True)  
                 return output
+            #Usual max-pooling operation over the features map
             elif self.pool == 'max':
                 output, _ = torch.max(output, 0,keepdim=True)
                 return output
@@ -67,7 +122,7 @@ class CountRegressor(nn.Module):
                     Output = torch.cat((Output,output),dim=0)
             return Output
 
-
+#The following two function are used to weight init
 def weights_normal_init(model, dev=0.01):
     if isinstance(model, list):
         for m in model:
